380f2766e8145fb19f053f1b7346fc7c91a77921
Hamza Macabato <hamzamacabato911@gmail.com>
2025-04-24 09:25:37 +0800

    first commit

---
+++ app/Livewire/ShowExpenses.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ShowExpenses extends Component
+{
+    public function render()
+    {
+        return view('livewire.show-expenses');
+    }
+}
