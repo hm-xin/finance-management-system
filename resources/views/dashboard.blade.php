61fd95c59c2a8b420d7841fce47ccce3699536b8
Hamza Macabato <hamzamacabato911@gmail.com>
2025-04-26 14:53:51 +0800

    Refactor expense creation form and dashboard link styles for improved UI consistency

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
-                    <a href="{{ route('createEditExpense') }}" class="text-blue-500 hover:underline">Add new Expenses</a>
+                    <a href="{{ route('createEditExpense') }}" class="whitespace-nowrap rounded-sm bg-black border border-black px-4 py-2 text-sm font-medium tracking-wide text-neutral-100 transition hover:opacity-75 text-center focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black active:opacity-100 active:outline-offset-0 disabled:opacity-75 disabled:cursor-not-allowed dark:bg-white dark:border-white dark:text-black dark:focus-visible:outline-white">Add new Expenses</a>
                     
                 </div>
             </div>
--- resources/views/livewire/create-edit-expense.blade.php
+++ resources/views/livewire/create-edit-expense.blade.php
@@ -7,17 +7,21 @@
 
     <form wire:submit.prevent="submit" x-data>
         <div class="flex w-full max-w-xs flex-col gap-1 text-neutral-600 dark:text-neutral-300">
-            <label for="textInputDefault" class="w-fit pl-0.5 text-sm">Name</label>
-            <input id="textInputDefault" type="text" class="w-full rounded-sm border border-neutral-300 bg-neutral-50 px-2 py-2 text-sm focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black disabled:cursor-not-allowed disabled:opacity-75 dark:border-neutral-700 dark:bg-neutral-900/50 dark:focus-visible:outline-white" name="name" placeholder="Enter your name" autocomplete="name"/>
+            <x-input-label for="textInputDefault" class="w-fit pl-0.5 text-sm">Name</x-input-label>
+            <x-text-input id="textInputDefault" type="text" class="w-full rounded-sm border border-neutral-300 bg-neutral-50 px-2 py-2 text-sm focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black disabled:cursor-not-allowed disabled:opacity-75 dark:border-neutral-700 dark:bg-neutral-900/50 dark:focus-visible:outline-white" name="name" placeholder="Enter your name" autocomplete="name"/>
         </div>
 
         <div class="flex w-full max-w-xs flex-col gap-1 text-neutral-600 dark:text-neutral-300">
-            <label for="textInputDefault" class="w-fit pl-0.5 text-sm">Name</label>
-            <input id="textInputDefault" type="text" class="w-full rounded-sm border border-neutral-300 bg-neutral-50 px-2 py-2 text-sm focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black disabled:cursor-not-allowed disabled:opacity-75 dark:border-neutral-700 dark:bg-neutral-900/50 dark:focus-visible:outline-white" name="name" placeholder="Enter your name" autocomplete="name"/>
+            <x-input-label for="textInputDefault" class="w-fit pl-0.5 text-sm">Name</x-input-label>
+            <x-text-input id="textInputDefault" type="text" class="w-full rounded-sm border border-neutral-300 bg-neutral-50 px-2 py-2 text-sm focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black disabled:cursor-not-allowed disabled:opacity-75 dark:border-neutral-700 dark:bg-neutral-900/50 dark:focus-visible:outline-white" name="name" placeholder="Enter your name" autocomplete="name"/>
         </div>
 
-        <x-primary-button type="submit" class="mt-4 w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition">
-            Submit
-        </x-primary-button>
+        <div class="flex items-center gap-4 mt-2">
+            <x-primary-button>{{ __('Save') }}</x-primary-button>
+
+            <x-action-message class="me-3" on="password-updated">
+                {{ __('Saved.') }}
+            </x-action-message>
+        </div>
     </form>
 </div>
